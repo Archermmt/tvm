65449581a4474a73c80ab2f695989cdf6a772852
Archermmt <archermmt@126.com>
2024-03-02 13:42:09 +0800

    change config for gallery

--- gallery/how_to/work_with_msc/using_tools.py
+++ gallery/how_to/work_with_msc/using_tools.py
@@ -54,11 +54,11 @@ parser.add_argument("--quantize", action="store_true", help="Whether to use quan
 parser.add_argument("--distill", action="store_true", help="Whether to use distiller for tool")
 parser.add_argument("--gym", action="store_true", help="Whether to use gym for tool")
 parser.add_argument("--test_batch", type=int, default=1, help="The batch size for test")
-parser.add_argument("--test_iter", type=int, default=2, help="The iter for test")
-parser.add_argument("--calibrate_iter", type=int, default=2, help="The iter for calibration")
-parser.add_argument("--train_batch", type=int, default=1, help="The batch size for train")
-parser.add_argument("--train_iter", type=int, default=2, help="The iter for train")
-parser.add_argument("--train_epoch", type=int, default=1, help="The epoch for train")
+parser.add_argument("--test_iter", type=int, default=100, help="The iter for test")
+parser.add_argument("--calibrate_iter", type=int, default=100, help="The iter for calibration")
+parser.add_argument("--train_batch", type=int, default=32, help="The batch size for train")
+parser.add_argument("--train_iter", type=int, default=200, help="The iter for train")
+parser.add_argument("--train_epoch", type=int, default=5, help="The epoch for train")
 args = parser.parse_args()
 
 
@@ -86,7 +86,7 @@ def get_config(calib_loader, train_loader):
         dataset=dataset,
         tools=tools,
         skip_config={"all": "check"},
-        verbose="debug:1",
+        verbose="info",
     )
 
 
